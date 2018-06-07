bb515f28768dc45ecfb6985d968fc77809740289
Carlos Rincon <mezerotm@gmail.com>
2018-06-06 23:41:32 -0400

    Removed express and added moleculer-web

--- moleculer/services/www.service.js
+++ moleculer/services/www.service.js
@@ -1,36 +1,52 @@
-const express = require('express')
-const cors = require('cors')
+const webService = require('moleculer-web')
 
 module.exports = {
   name: 'www',
+  mixins: [webService],
   settings: {
-    port: process.env.PORT || 3001
+    port: process.env.SERVER_PORT || 3001,
+    cors: true,
+    // authorization: true,
+    routes: [{
+      aliases: {
+        '': 'www.index',
+        'POST users': 'users.create'
+      }
+    }]
   },
   methods: {
-    initRoutes(app) {
-      app.get('/', this.index)
-    },
-    index(req, res) {
-      res.send({
-        message: 'Hello from moleculer + express'
-      })
-    }
-  },
-  created() {
-    const app = express()
-
-    app.use(cors())
-
-    this.initRoutes(app)
-    this.app = app
-  },
-  started() {
-    this.app.listen(this.settings.port, error => {
-      if(error) return this.broker.fatal(error)
-      this.logger.info(`Listening on: ${this.settings.port}`)
-    })
-  },
-  stopped() {
-
+    // authorize(ctx, route, req) {
+		// 	let token;
+		// 	if (req.headers.authorization) {
+		// 		let type = req.headers.authorization.split(" ")[0];
+		// 		if (type === "Token" || type === "Bearer")
+		// 			token = req.headers.authorization.split(" ")[1];
+		// 	}
+    //
+		// 	return this.Promise.resolve(token)
+		// 		.then(token => {
+		// 			if (token) {
+		// 				// Verify JWT token
+		// 				return ctx.call("users.resolveToken", { token })
+		// 					.then(user => {
+		// 						if (user) {
+		// 							this.logger.info("Authenticated via JWT: ", user.username);
+		// 							// Reduce user fields (it will be transferred to other nodes)
+		// 							ctx.meta.user = _.pick(user, ["_id", "username", "email", "image"]);
+		// 							ctx.meta.token = token;
+		// 						}
+		// 						return user;
+		// 					})
+		// 					.catch(err => {
+		// 						// Ignored because we continue processing if user is not exist
+		// 						return null;
+		// 					});
+		// 			}
+		// 		})
+		// 		.then(user => {
+		// 			if (req.$endpoint.action.auth == "required" && !user)
+		// 				return this.Promise.reject(new UnAuthorizedError());
+		// 		});
+		// }
   }
 }
