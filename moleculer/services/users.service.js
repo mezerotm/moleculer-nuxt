d2073d63364b400794410c3e561a9f06a9680d69
Carlos Rincon <mezerotm@gmail.com>
2018-06-16 15:31:12 -0400

    moved authorization to separate branch

--- moleculer/services/users.service.js
+++ moleculer/services/users.service.js
@@ -1,7 +1,6 @@
 const dbService = require('moleculer-db')
 const mongooseAdapter = require('moleculer-db-adapter-mongoose')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 module.exports = {
   name: 'users',
@@ -11,31 +10,5 @@ module.exports = {
   mixins: [dbService],
   adapter: new mongooseAdapter(process.env.DATABASE_URI || "mongodb://localhost:27017"),
   model: User,
-  actions: {
-    // resolveToken: {
-		// 	cache: {
-		// 		keys: ["token"],
-     //    // 1 hour
-		// 		ttl: 60 * 60
-		// 	},
-		// 	params: {
-		// 		token: "string"
-		// 	},
-		// 	handler(ctx) {
-     //    try {
-     //      //TODO: could be promise based, try with 'await' once confirmed with callback
-     //      jwt.verify(ctx.params.token, this.settings.JWT_SECRET, (error, decoded) => {
-     //        if(error) return console.error(error)
-     //        if(decoded.id) {
-     //          return this.getById(decoded.id)
-     //        } else {
-     //          return console.error('no decoded.id')
-     //        }
-     //      })
-     //    } catch(error) {
-     //      return console.error(error)
-     //    }
-		// 	}
-		// }
-  }
+  actions: {}
 }
--- moleculer/services/www.service.js
+++ moleculer/services/www.service.js
@@ -14,31 +14,5 @@ module.exports = {
       }
     }]
   },
-  methods: {
-    // async authorize(ctx, route, req) {
-     //  //TODO: refactor
-     //  let token;
-		// 	if (req.headers.authorization) {
-		// 		let type = req.headers.authorization.split(" ")[0];
-		// 		if (type === 'Token' || type === 'Bearer') {
-		// 			token = req.headers.authorization.split(" ")[1];
-     //    }
-		// 	}
-    //
-     //  try {
-     //    const user = await ctx.call('users.resolveToken', {token})
-     //    if(user) {
-     //      this.logger.info(`authenticated jwt: ${user._id}`)
-     //      ctx.mata.user = users
-     //      ctx.mera.token = token
-     //    } else {
-     //      //TODO: figure out what to do
-     //      return console.error('no user')
-     //    }
-     //  } catch (error) {
-     //    //TODO: figure out how to properly log this
-     //    return console.error(error)
-     //  }
-		// }
-  }
+  methods: {}
 }
