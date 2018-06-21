fc9454bc041e8666c61e20e675584cf39ab03aaa
Carlos Rincon <mezerotm@gmail.com>
2018-06-21 02:28:10 -0400

    Added buefy & pug & sass

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -2,19 +2,17 @@ module.exports = {
   head: {
     title: 'moleculer + nuxt.js',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ]
   },
   modules: [
-    '@nuxtjs/axios'
+    ['@nuxtjs/axios', {baseURL: process.env.BASE_URL || 'http://localhost:3001'}],
+    'nuxt-buefy'
   ],
-  axios: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3001'
-  },
   build: {
-    extend (config, { isDev, isClient }) {
+    extend (config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
